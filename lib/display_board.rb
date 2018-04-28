d1b0516720fb5cc15a29112b59ed2ee2f0ff5feb
sduome@yahoo.com <sduome@yahoo.com>
2018-04-28 06:11:26 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
   print ["  |", "  |", "  |"]
-  3.times puts ["  |", "  |", "  |"]
+  2.times puts ["  |", "  |", "  |"]
 end
 prints display_board
